e158f0ddb878621ffe0cf573a1005068fc32a0c7
John Scholvin <john@scholvin.com>
2022-04-20 20:00:04 -0500

    Initial Commit

---
+++ TBGB Control/TBGB_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBGB_ControlApp.swift
+//  TBGB Control
+//
+//  Created by John Scholvin on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct TBGB_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TBGB ControlUITests/TBGB_ControlUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TBGB_ControlUITestsLaunchTests.swift
+//  TBGB ControlUITests
+//
+//  Created by John Scholvin on 4/20/22.
+//
+
+import XCTest
+
+class TBGB_ControlUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
