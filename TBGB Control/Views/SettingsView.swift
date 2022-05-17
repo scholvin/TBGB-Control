8a12ceda587c21988ac55820fd04e5e194626596
John Scholvin <john@scholvin.com>
2022-05-16 19:05:17 -0500

    SettingsView looking a little better

--- TBGB Control/Views/SettingsView.swift
+++ TBGB Control/Views/SettingsView.swift
@@ -11,19 +11,40 @@ import SwiftUI
 
 struct SettingsView: View {
     @Binding var activeSheet: Sheet?
+    @State var oladEnabled: Bool = false
+    @State var oladAddress: String = "192.168.1.100:9090"
     
     var body: some View {
-        VStack(spacing: 50) {
-            Text("Settings view.")
+        VStack() {
+           Text("Settings")
                 .font(.largeTitle)
-            
+                .padding(5)
+
+            Form {
+                Section(header: Text("OLAD")) {
+                    Toggle("Active", isOn: $oladEnabled)
+                    HStack() {
+                        Text("IP")
+                        Spacer()
+                        TextField("IP", text: $oladAddress).multilineTextAlignment(.trailing)
+                    }
+                }
+            }
+            .frame(height: 200)
+                
             Button(action: {
                 activeSheet = nil
             }, label: {
-                Label("Close", systemImage: "xmark.circle")
+                Label("OK", systemImage: "checkmark")                    
             })
+            .frame(width: 125, height: 50)
+            .background(Color.blue.cornerRadius(8))
+            .foregroundColor(.white)
+
+            Spacer()
+        
         }
-        .background(Color.green)
+        .foregroundColor(.black)
     }
 }
 
